4aec790a80a95d3a65ed129b920d770e628c807d
Subhojit Paul <subhojitpaul21@gmail.com>
2016-11-27 18:17:22 +0530

    users not required

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
